90c8b3149f12157e181f097a6e48117d7b06c5f5
anthony <ajdt703@gmail.com>
2021-12-07 08:30:47 -0800

    Add day 7 part 1 solution

--- 07/src/main.rs
+++ 07/src/main.rs
@@ -1,3 +1,22 @@
+use std::str::FromStr;
+
 fn main() {
-    println!("Hello, world!");
+    let data = std::fs::read_to_string("../data/day_07.txt")
+        .expect("Failed to open data file");
+    let mut crab_positions = data.trim().split(',')
+        .map(|position_raw| i32::from_str(position_raw).unwrap())
+        .collect::<Vec<i32>>();
+    crab_positions.sort();
+
+    // -- Part 1 --
+    let median_index = crab_positions.len() / 2;
+    let median = match crab_positions.len() % 2 == 0 {
+        true => (crab_positions[median_index] + crab_positions[median_index - 1]) / 2, 
+        false => crab_positions[median_index],
+    };
+    
+    let total_fuel = crab_positions.iter()
+        .fold(0, |fuel_cost, curr| fuel_cost + (median - curr).abs());
+    println!("total fuel cost for aligning crabs = {}", total_fuel);
+
 }

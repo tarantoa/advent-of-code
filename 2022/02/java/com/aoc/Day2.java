d245084d9159090612ed7b64ee73c588edd16fa9
Anthony Di Taranto <ajdt703@gmail.com>
2022-12-02 10:51:35 -0500

    Add solution for day 2 part 2

--- 2022/02/java/com/aoc/Day2.java
+++ 2022/02/java/com/aoc/Day2.java
@@ -14,29 +14,46 @@ public class Day2 {
   private static final int TIE_POINTS = 3;
 
   private enum Hand {
-    Rock(ROCK, 1, SCISSORS),
-    Paper(PAPER, 2, ROCK),
-    Scissors(SCISSORS, 3, PAPER);
+    Rock(ROCK, 1, SCISSORS, PAPER),
+    Paper(PAPER, 2, ROCK, SCISSORS),
+    Scissors(SCISSORS, 3, PAPER, ROCK);
 
     private final String name;
     private final int value;
     private final String beats;
+    private final String losesTo;
 
-    private Hand(final String name, final int value, final String beats) {
+    private Hand(final String name, final int value, final String beats, final String losesTo) {
       this.name = name;
       this.value = value;
       this.beats = beats;
+      this.losesTo = losesTo;
     }
 
-    public boolean doesBeat(Hand other) {
+    public boolean doesBeat(final Hand other) {
       return other.name.equals(beats);
     }
 
-    public static Hand decode(String code) {
+    public static Hand decode(final String code) {
+      if (code == null || code.isEmpty()) throw new IllegalArgumentException("code cannot be blank.");
       if (code.equals("A") || code.equals("X")) return Rock;
       if (code.equals("B") || code.equals("Y")) return Paper;
       if (code.equals("C") || code.equals("Z")) return Scissors;
-      throw new IllegalArgumentException("invalid hand code.");
+      throw new IllegalArgumentException("invalid hand code. { " + code + " }");
+    }
+
+    public static Hand fromString(final String name) {
+      if (name == null || name.isEmpty()) throw new IllegalArgumentException("name cannot be blank.");
+      if (name.equals(ROCK)) return Rock;
+      if (name.equals(PAPER)) return Paper;
+      if (name.equals(SCISSORS)) return Scissors;
+      throw new IllegalArgumentException("invalid hand name. { " + name + " }");
+    }
+
+    public static Hand decode2(final Hand other, final String code) {
+      if (code.equals("X")) return fromString(other.beats);
+      if (code.equals("Y")) return other;
+      return fromString(other.losesTo);
     }
   }
   
@@ -51,31 +68,40 @@ public class Day2 {
   }
   
   public static void main(String[] args) {
-    int elfScore1 = 0; 
-    int elfScore2 = 0;
+    GameResult resultPart1 = new GameResult(0, 0);
+    GameResult resultPart2 = new GameResult(0, 0);
     try (BufferedReader br = new BufferedReader(new FileReader(DATA_FILEPATH))) {
       String line;
       while ((line = br.readLine()) != null) {
         String[] handCodes = line.split(" ");
-        Hand elfHand1 = Hand.decode(handCodes[0]); Hand elfHand2 = Hand.decode(handCodes[1]);
-        GameResult result = playHands(elfHand1, elfHand2);
-        elfScore1 += result.playerPoints1;
-        elfScore2 += result.playerPoints2;
+        Hand elfHand1 = Hand.decode(handCodes[0]);
+        Hand elfHand2 = Hand.decode(handCodes[1]);
+        Hand elfHand2Part2 = Hand.decode2(elfHand1, handCodes[1]);
+        resultPart1.combine(playHands(elfHand1, elfHand2));
+        resultPart2.combine(playHands(elfHand1, elfHand2Part2));
       }
     } catch (Exception e) {
       e.printStackTrace();
     }
-    System.out.printf("Scores { Player1: %d, Player2: %d }\n", elfScore1, elfScore2); // Part 1
+    System.out.printf(
+      "Scores { Player1: %d, Player2: %d }\n", resultPart1.playerPoints1, resultPart1.playerPoints2); // Part 1
+    System.out.printf(
+      "Scores { Player1: %d, Player2: %d }\n", resultPart2.playerPoints2, resultPart2.playerPoints2); // Part 2
   }
 
   private static final class GameResult {
     
-    private final int playerPoints1;
-    private final int playerPoints2;
+    private int playerPoints1;
+    private int playerPoints2;
 
     private GameResult(final int playerPoints1, final int playerPoints2) {
       this.playerPoints1 = playerPoints1;
       this.playerPoints2 = playerPoints2;
+    }
+
+    private void combine(final GameResult other) {
+      this.playerPoints1 += other.playerPoints1;
+      this.playerPoints2 += other.playerPoints2;
     }
   }
 }

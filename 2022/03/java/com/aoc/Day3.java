a460b57aa35c1a7301a7687948279571555338c1
Anthony Di Taranto <ajdt703@gmail.com>
2022-12-03 13:57:48 -0500

    Add day 3 part 1 solution

---
+++ 2022/03/java/com/aoc/Day3.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.stream.Collectors;
+
+public class Day3 {
+
+  private static final String DISABLE_TEST_FLAG = "--notest";
+
+  private static final String DATA_FILEPATH = "../../data/day_03.txt";
+  private static final String TEST_DATA_FILEPATH = "../../data/day_03_test.txt";
+
+  public static void main(String[] args) {
+    String dataDir = TEST_DATA_FILEPATH;
+    if (args.length == 1 && args[0].equals(DISABLE_TEST_FLAG)) {
+      dataDir = DATA_FILEPATH;
+    }
+
+    try (BufferedReader br = new BufferedReader(new FileReader(dataDir))) {
+      long prioritySum = br.lines()
+        .map(line -> {
+          int middleIndex = line.length() / 2;
+          String leftHalf = line.substring(0, middleIndex);
+          String rightHalf = line.substring(middleIndex);
+          return rightHalf.chars()
+            .mapToObj(i -> Character.toString((char)i))
+            .filter(leftHalf::contains)
+            .collect(Collectors.toSet())
+            .stream()
+            .reduce("", (union, currentChar) -> union + currentChar);
+          })
+        .map(union -> union.chars()
+          .reduce(0, (sum, currentCharacter) -> {
+            if (isLowerCase(currentCharacter)) return sum + (currentCharacter - '`');
+            if (isUpperCase(currentCharacter)) return sum + (currentCharacter - '&');
+            return sum;
+          }))
+        .reduce(0, (cumSum, currentLineSum) -> cumSum + currentLineSum);
+        System.out.printf("Priority Sum: %d\n", prioritySum);
+      br.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static final boolean isUpperCase(final int character)  {
+    return 65 <= character && character <= 90;
+  }
+
+  private static final boolean isLowerCase(final int character) {
+    return 97 <= character && character <= 122;
+  }
+}

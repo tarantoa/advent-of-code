b7114507dab79ad2600443e73c7c84c546ffd91d
Anthony Di Taranto <ajdt703@gmail.com>
2022-12-03 21:29:56 -0500

    Remove unused imports

--- 2022/03/java/com/aoc/Day3Part2.java
+++ 2022/03/java/com/aoc/Day3Part2.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collector;
 
 public class Day3Part2 {

eddc3e59b7839ffaa5edd47f66caf47cd63b69e9
Anthony Di Taranto <ajdt703@gmail.com>
2022-12-07 11:47:06 -0500

    Refactor day 1 solution

--- 2022/01/java/com/aoc/Day1.java
+++ 2022/01/java/com/aoc/Day1.java
@@ -3,44 +3,58 @@ package aoc;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Day1 {
 
   private static final String TEST_DATA_FILEPATH = "../../testdata/day_01.txt";
   private static final String DATA_FILEPATH = "../../data/day_01.txt";
 
-  public static List<Long> readCalorieCountsFromfile() {
-    List<Long> elfCalories = new ArrayList<>();
-    try (BufferedReader br = new BufferedReader(new FileReader(DATA_FILEPATH))) {
-      String line;
-      Long calorieCount = 0L;
-      while ((line = br.readLine()) != null)  {
-        if (line.isEmpty()) {
-          elfCalories.add(calorieCount);
-          calorieCount = 0L;
-        } else {
-          calorieCount += Long.parseLong(line);
+  private static Collector<String, List<List<String>>, List<List<String>>> toSplitList(final String delimiter) {
+    return Collector.of(
+      ArrayList<List<String>>::new,
+      (splitList, item) -> {
+        List<String> list = splitList.isEmpty() ? null : splitList.get(splitList.size() - 1);
+        if (list == null || item.equals(delimiter)) {
+          splitList.add(list = new ArrayList<>());
+          return;
         }
-      }
-      br.close();
-      elfCalories.add(calorieCount);
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    return elfCalories;
+        list.add(item);
+      },
+      (r1, r2) -> { throw new UnsupportedOperationException("Parallel processing not supported."); }
+    );
   }
 
   public static void main(String[] args) {
-    List<Long> elfCalories = readCalorieCountsFromfile();
-    Collections.sort(elfCalories);
-    Collections.reverse(elfCalories);
-    System.out.printf("Most calories: %d\n", elfCalories.get(0)); // Part 1
-    if (elfCalories.size() < 3) {
-      throw new IllegalStateException("there must be at least three calorie entries.");
+    final String filepath = 
+      args.length == 1 && args[0].equals("--notest") ? DATA_FILEPATH : TEST_DATA_FILEPATH;
+    List<Long> calorieTotals = new ArrayList<>();
+    try (BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
+      calorieTotals = reader.lines()
+        .collect(toSplitList(""))
+        .stream()
+        .map(foodItems -> foodItems.stream()
+          .map(Long::parseLong)
+          .reduce(0L, Long::sum))
+        .collect(Collectors.toList());
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-    System.out.printf("Calories from top 3 elves: %d\n", 
-      elfCalories.get(0) + elfCalories.get(1) + elfCalories.get(2));
+
+    if (calorieTotals.isEmpty()) {
+      System.out.println("Result calorie list is empty.");
+      return;
+    }
+
+    Collections.sort(calorieTotals);
+    Collections.reverse(calorieTotals);
+
+    System.out.printf("Highest calorie count: %d\n", calorieTotals.get(0)); // Part 1
+    System.out.printf("Sum of the highest three calorie counts: %d\n",
+      calorieTotals.get(0) + calorieTotals.get(1) + calorieTotals.get(2));
   }
 }
